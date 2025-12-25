8107bfaac0aa8dcd69b69d9aa914444a62d732c4
Anton Kuzan <antonkuzan@gmail.com>
2025-12-25 19:46:09 +0300

    Connect UI signals to Main scene
    feat: connect timer control signals to main scene Implement signals for Play, Pause, and Stop buttons and handle them in Main

--- Scenes/All Timers/all_timers_general_scene.gd
+++ Scenes/All Timers/all_timers_general_scene.gd
@@ -3,12 +3,25 @@ extends Node2D
 @onready var work_timer_scene: Node2D = $work_timer_scene
 @onready var timer_controls: Control = $timer_controls
 
-#var play_button : Button = 
+enum State { STOPPED, RUNNING, PAUSED }
+var state: State = State.STOPPED
 
 
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
+
+
+func _on_timer_controls_stopped() -> void:
+	print("Стоп нажат!")
+
+
+func _on_timer_controls_running() -> void:
+	print("Плей нажат!")
+
+
+func _on_timer_controls_paused() -> void:
+	print("Пауза нажата!")
--- UI/timer_controls.gd
+++ UI/timer_controls.gd
@@ -1,19 +1,33 @@
 extends Control
 
+signal stopped
+signal running
+signal paused
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
 
+#func _process(delta: float) -> void:
+	#pass
+
+
 func _on_reduce_minutes_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_add_minutes_pressed() -> void:
-	pass # Replace with function body.
+	pass
+
+
+func _on_play_pressed() -> void:
+	emit_signal("running")
+
+
+func _on_stop_pressed() -> void:
+	emit_signal("stopped")
+
+
+func _on_pause_pressed() -> void:
+	emit_signal("paused")
