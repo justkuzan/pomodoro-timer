5284ff312419b7f5546087a045a041b521f17bb5
Anton Kuzan <antonkuzan@gmail.com>
2025-12-27 17:14:06 +0300

    Refactor state management and add Ended state
    Added ENDED state to handle timer completion events.
    
    Implemented change_state function to centralize all state transitions.

--- Scenes/All Timers/all_timers_general_scene.gd
+++ Scenes/All Timers/all_timers_general_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var work_timer: Node2D = $work_timer_scene
 @onready var timer_controls: Control = $timer_controls
 
-enum State { STOPPED, RUNNING, PAUSED }
+enum State { STOPPED, RUNNING, PAUSED, ENDED }
 var state: State = State.STOPPED
 
 var user_minutes: int = 12
@@ -15,26 +15,39 @@ func _ready() -> void:
 	pass
 	
 	
+func change_state(new_state: State) -> void:
+	state = new_state
+	match state:
+		State.RUNNING:
+			print("Плей нажат!")
+			work_timer.timer_started()
+		State.STOPPED:
+			print("Стоп нажат!")
+			work_timer.timer_stopped()
+			work_timer.set_time(user_minutes, user_seconds)
+		State.PAUSED:
+			print("Пауза нажата!")
+			work_timer.timer_paused()
+		State.ENDED:
+			print("Время и стекло")
+			
+	
 func set_time_in_timer() -> void:
 	work_timer.set_time(user_minutes, user_seconds)
 	pass
 	
 	
 func _on_timer_controls_running() -> void:
-	print("Плей нажат!")
-	work_timer.timer_started()
+	change_state(State.RUNNING)
 	
 	
 func _on_timer_controls_stopped() -> void:
-	print("Стоп нажат!")
-	work_timer.timer_stopped()
-	work_timer.set_time(user_minutes, user_seconds)
+	change_state(State.STOPPED)
 	
 	
 func _on_timer_controls_paused() -> void:
-	print("Пауза нажата!")
-	work_timer.timer_paused()
+	change_state(State.PAUSED)
 	
 	
 func _on_work_timer_scene_timer_finished() -> void:
-	pass
+	change_state(State.ENDED)
