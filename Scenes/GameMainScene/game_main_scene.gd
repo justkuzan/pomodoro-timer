8d2ecdbdd3373b750ce044e5a622318b8ddbef1c
Anton Kuzan <antonkuzan@gmail.com>
2026-01-02 17:36:25 +0300

    Add custom mouse cursor and assets
    Added custom cursor texture.
    
    Configured hardware cursor in project settings.

--- Scenes/GameMainScene/game_main_scene.gd
+++ Scenes/GameMainScene/game_main_scene.gd
@@ -11,5 +11,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
